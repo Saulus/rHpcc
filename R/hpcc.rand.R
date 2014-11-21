557235d4ba991dd026cafc9b2d189737588b7a63
Paul Hellwig <p.hellwig@elsevier.com>
2014-11-21 12:52:10 +0100

    missing parenthesis

--- R/hpcc.rand.R
+++ R/hpcc.rand.R
@@ -9,7 +9,7 @@ hpcc.rand <-
 		xyz<-sprintf("%s%s;)\n",xyz,dataframe)
 		xyz<-sprintf("%sEND;\n",xyz)
 		MyTrans <- hpcc.get.name()
-		xyz<-paste(xyz,paste("%s %s(%s L, UNSIGNED4 C) := TRANSFORM\n",MyOutRec,MyTrans,dataframe)
+		xyz<-paste(xyz,paste("%s %s(%s L, UNSIGNED4 C) := TRANSFORM\n",MyOutRec,MyTrans,dataframe))
 		xyz<-sprintf("%sSELF.rand := C/4294967295;\n",xyz)
 		xyz<-sprintf("%sSELF := L;\n",xyz)
 		xyz<-sprintf("%sEND;\n",xyz)
